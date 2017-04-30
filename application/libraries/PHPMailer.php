54ff51662f3b0afcb62c62adf533308021c6d180
Ever Leiton <everleitonm@gmail.com>
2017-04-29 23:26:34 -0600

    Modificación de página de vista de mis productos

--- application/libraries/PHPMailer.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
- 
-class PHPMailer {
-    public function My_PHPMailer() {
-        require_once('PHPMailer/class.phpmailer.php');
-    }
-}
