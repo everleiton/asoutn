8802e15742fa59c4040f9bf89c2673bc29f0e166
Ever Leiton <everleitonm@gmail.com>
2017-05-02 19:01:38 -0600

    Cambios

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,59 +1,58 @@
 <?php
 
 class User_model extends CI_Model{
-
+	/**
+	* METODO PARA AGREGAR USUARIO
+	*
+	*/
 	function insertar($user)
 	{
 		$query = $this->db->insert('usuarios',$user);
-
+		
 		return $query;
 	}
-	
+	/**
+	* METODO PARA AGREGAR FOTO DEL USUARIO
+	*
+	*/
 	function insertarfoto($foto)
 	{
 		$query= "INSERT INTO imagen_perfil (nombre, imagen, id_usuario) VALUES('$nombre', '$Imagen', 'ever')";
-	 
-	  $resultado =  $this->db->query($query);
-	}
-	function login($email, $password)
-	{
 		
-
+		$resultado =  $this->db->query($query);
+	}
+	
+	/**
+	* METODO PARA INICIAR SESION
+	*
+	*/
+	function login($email, $password)
+	{/*
 		$this->db->select('id, email, password, name, photo, tipousuario');
 		$this ->db-> from('usuarios');
-		
 		$this ->db-> where('email', $username);
 		$this ->db-> where('pass', $password );
 		$this ->db-> limit(1);
 		$query = $this -> db -> get();
 		if($query -> num_rows() == 1)
 		{
-			return $query->result();
-		}
-		else
-		{
-			return false;
-		}
+		return $query->result();
 	}
-	 function authenticate($user, $pass) {
-		 $query = $this->db->get_where('usuarios', array('email' => $user, 'password' => $pass));
-		
-	//return $query->result_object();
-	  return $query;
-  }
+	else
+	{
+	return false;
+}
+}
+**/
+/**
+* METODO PARA AUTENTICAR
+*
+*/
+function authenticate($user, $pass) {
+	$query = $this->db->get_where('usuarios', array('email' => $user, 'password' => $pass));
+	
+	return $query;
+}
 
-	/*
-	Actualizar datos
-	*/
-    function actualizar_user($id, $email, $password, $name, $photo) {
-        $data = array(
-             'fullname' => $name,
-           'username' => $username,
-            'pass' => $pass,
-            'email' => $email,
-            'address' => $address
-        );
-        $this->db->where('id', $id);
-        return $this->db->update('usuarios', $data);
-    }
+
 }
